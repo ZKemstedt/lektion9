0af8ce3e8d7cfdd0cd7674a13912d82aee965072
Robert Westin <70424136+robert-alfwar@users.noreply.github.com>
2020-09-10 01:57:41 +0200

    add write example

--- exempel/filhantering.py
+++ exempel/filhantering.py
@@ -3,3 +3,9 @@ try:
         print(f)
 except FileNotFoundError:
     print("File not found")
+
+
+persons = ["kalle", "lisa", "stina", "olle"]
+with open("a_new_file", "w+") as w:
+    for person in persons:
+        w.write(f'{person}\n')
